8b9777ce7a1c61037c9d3b2bf14c2db752268195
Wasim Akram <mr.saim516@gmail.com>
2025-02-23 23:59:29 +0500

    update / media file size supported upto 50 mbs

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -19,7 +19,7 @@ const fileFilter = (req, file, cb) => {
 // ✅ Allow multiple files (max 5 at a time)
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Max 10MB per file
+  limits: { fileSize: 50 * 1024 * 1024 }, // Max 50MB per file
   fileFilter,
 });
 
